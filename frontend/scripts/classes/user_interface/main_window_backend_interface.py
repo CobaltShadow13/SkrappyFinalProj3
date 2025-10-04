a44224faa639f170fc22d1b0a2cb26da39bc0b9d
CobaltShadow13 <62352915+CobaltShadow13@users.noreply.github.com>
2025-10-04 03:33:59 -0600

    Success
    Backend has successfully been passed into the frontend and the front end is recieving data. Problem of how to make those images display is still somewhat of a question but I will be researching
    
    Backend is passed completely through and accessable by the frontend. Frontend work to be done consists of like... figuing out how to display the images. Making the pixelart for the stuff, and going from there.
    
    The backend still needs a LOT of cleanup work, and also to be integrated with the board pieces created when the program is running. All in all huge progress. And we will end up with something more complete here soon hopefully

--- frontend/scripts/classes/user_interface/main_window_backend_interface.py
+++ frontend/scripts/classes/user_interface/main_window_backend_interface.py
@@ -1,6 +1,16 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QTableWidgetItem
+from PyQt6.QtWidgets import QHeaderView
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QTableWidgetItem, QTableWidget, QHeaderView
 from backend.scripts.utilities.board_utilities.board_utilities import create_board_piece
 from frontend.scripts.classes.user_interface.main_window_class import Ui_MainWindow
+
+
+class TileTableWidgetItem(QTableWidgetItem):
+    def __init__(self, tile):
+        super().__init__()
+        self.setText(f"({int(tile.get_x_tile_coord())},{int(tile.get_y_tile_coord())})")
+        self.tile = tile
+    def get_tile(self):
+        return self.tile
 
 class MainWindow(QMainWindow):
     def __init__(self, backend):
@@ -11,6 +21,7 @@ class MainWindow(QMainWindow):
         self.ui.create_board_piece_button.clicked.connect(self.on_create_board_piece)
         self.populate_families()
         self.populate_shapes()
+        self.populate_ui_grid()
 
     def add_to_bp_list(self, board_piece):
         bptw = self.ui.board_piece_table_widget
@@ -28,8 +39,6 @@ class MainWindow(QMainWindow):
         create_board_piece(board, name, apriltag_family, shape)
         self.add_to_bp_list(board.get_board_piece_array()[-1])
 
-
-
     def populate_families(self): ##adds all tag families in the current board to the options menu
         for fam in self.backend.get_families():
             self.ui.apriltag_family_input_box.addItem(fam.get_tag_family(), fam)
@@ -37,6 +46,49 @@ class MainWindow(QMainWindow):
     def populate_shapes(self):
         for shape in self.backend.get_shapes():
             self.ui.board_piece_shape_input_box.addItem(shape.get_name(), shape)
+
+    def transfer_tile_id_to_table_widget_coords(self, tile_id):
+        width = self.backend.get_board().get_grid().get_width()
+        col = tile_id / width
+        row_bottom_up = tile_id % width
+        row = (width - 1) - row_bottom_up
+
+        return row, col
+
+    def make_cells_square(self, table):
+        table_width = table.viewport().width()
+        table_height = table.viewport().height()
+
+        cell_width = table_width / table.columnCount()
+        cell_height = table_height / table.rowCount()
+        side = min(cell_width, cell_height)+100
+
+        for x in range(table.columnCount()):
+            table.setColumnWidth(x, side)
+        for y in range(table.rowCount()):
+            table.setRowHeight(y, side)
+
+    def populate_ui_grid(self):
+        bgtw = self.ui.board_grid_table_widget
+        width = self.backend.get_board().get_grid().get_width()
+        height = self.backend.get_board().get_grid().get_height()
+        bgtw.setRowCount(height)
+        bgtw.setColumnCount(width)
+
+
+        tile_widgets = []
+        for tile in self.backend.get_board().get_grid().get_tile_map():
+            tile_widgets.append(TileTableWidgetItem(tile))
+        for tile_widget in tile_widgets:
+            tile_id = tile_widget.get_tile().get_tile_id()
+            row, col = self.transfer_tile_id_to_table_widget_coords(tile_id)
+            bgtw.setItem(row, col, tile_widget)
+        bgtw.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        bgtw.verticalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.make_cells_square(bgtw)
+
+
+
 
 
 
